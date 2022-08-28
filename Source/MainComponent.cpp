a141786aa0245c1cf5ee58867ce25c2ac6454a9b
kornhausersound <112279112+kornhausersound@users.noreply.github.com>
2022-08-28 00:48:41 -0400

    updated size

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,7 +16,7 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (48.0f));
+    g.setFont (juce::Font (60.0f));
     g.setColour (juce::Colours::red);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
